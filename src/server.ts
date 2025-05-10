5a1b6d0d79243935656d457fab943b27dfcb7afb
Enrico M Caliari <enricomassariol@gmail.com>
2025-05-10 19:44:11 -0300

    fix: use 0.0.0.0 as host for Render

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,8 @@ import { env } from './env'
 app
   .listen({
     port: env.PORT,
+    host: '0.0.0.0',
   })
   .then(() => {
-    console.log('HTTP Server Running!')
+    console.log(`HTTP Server Running on port ${env.PORT}`)
   })
